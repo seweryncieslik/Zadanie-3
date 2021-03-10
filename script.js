073dea2bc608f5b7f2412e0a74f18597d2f7e0e9
Seweryn Cieślik <seweryn.cieslik@gmail.com>
2021-03-10 09:56:17 +0100

    Operator change

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ol = document.body.querySelector('ol');
     for (i = 1; i<=11; i++) {
         let li = document.createElement('li');
         li.textContent = `Seweryn ${i}`;
-        if (i % 2 == 0) {
+        if (i % 2 === 0) {
             li.style.background="red";
             ol.appendChild(li);
         }
