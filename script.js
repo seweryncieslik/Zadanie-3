a900445adba95e157b81f55d6c0a6ea9f506274e
Seweryn Cieślik <seweryn.cieslik@gmail.com>
2021-03-10 09:53:00 +0100

    Init

---
+++ script.js
@@ -0,0 +1,17 @@
+var i;
+const ol = document.body.querySelector('ol');
+    
+    for (i = 1; i<=11; i++) {
+        let li = document.createElement('li');
+        li.textContent = `Seweryn ${i}`;
+        if (i % 2 == 0) {
+            li.style.background="red";
+            ol.appendChild(li);
+        }
+        else
+        {
+            ol.appendChild(li);
+        }
+
+    }
+    
